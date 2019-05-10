b3b8c8a65b5628ae6481c26cf6b09a6e9ac6a406
maidoushaoxia <shaoyun5429@163.com>
2019-05-10 10:34:21 +0800

    updated

---
+++ Easy/122.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var maxProfit = 0;
+    for(var i=0;i<prices.length;i++){
+      //只要后一天比前一天大,就卖出.
+      if(prices[i+1] > prices[i]){
+        maxProfit += prices[i+1] - prices[i];
+      }
+    }
+    return maxProfit;
+};
