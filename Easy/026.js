bb2de6626391b967301637f548ca00d3e14acf83
maidoushaoxia <shaoyun5429@163.com>
2019-09-07 19:53:25 +0800

    【+】24、26及27题

--- Easy/026.js
+++ Easy/026.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: shaoyun
+ * @Date: 2019-04-26 10:22:25
+ * @LastEditors: shaoyun
+ * @LastEditTime: 2019-09-07 19:28:33
+ * @Description: 删除排序数组中的重复项
+ */
 /**
  * @param {number[]} nums
  * @return {number}
@@ -17,4 +24,23 @@ var removeDuplicates = function(nums) {
     }
   }
   return nums.length;
+};
+
+/**
+ * @description: 双指针思想
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0
+    let i = 0 // i是慢指针
+    for (let j = 1;j < nums.length;j++) { // j是快指针
+      // 快指针跳过重复项，若两个指针对应的值不同，则将快指针的值赋给慢指针
+      if (nums[i] !== nums[j]) {
+        i ++
+        nums[i] = nums[j]
+      }
+    }
+    // 最后返回慢指针所在的索引，这样数组就是不重复的了
+    return i + 1
 };
--- Easy/027.js
+++ Easy/027.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: shaoyun
+ * @Date: 2019-04-26 10:35:02
+ * @LastEditors: shaoyun
+ * @LastEditTime: 2019-09-07 19:53:06
+ * @Description: 移除元素
+ */
 /**
  * @param {number[]} nums
  * @param {number} val
@@ -14,4 +21,22 @@ var removeElement = function(nums, val) {
       }
     }
     return nums.length;
+};
+
+/**
+ * @description: 还是双指针思想
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    if (nums.length === 0) return 0
+    let i = 0
+    for (let j = 0;j < nums.length;j++) {
+      if (nums[j] !== val) {
+        nums[i] = nums[j]
+        i ++
+      }
+    }
+    return i
 };
