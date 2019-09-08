585740fc20c603ed13eb2c41de82bd3097a8717c
maidoushaoxia <shaoyun5429@163.com>
2019-09-08 18:51:09 +0800

    【*】28题

--- Easy/028.js
+++ Easy/028.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: shaoyun
+ * @Date: 2019-04-26 11:13:50
+ * @LastEditors: shaoyun
+ * @LastEditTime: 2019-09-08 18:50:49
+ * @Description: 实现strStr()
+ */
 /**
  * @param {string} haystack
  * @param {string} needle
@@ -9,3 +16,20 @@ var strStr = function(haystack, needle) {
     }
     return haystack.indexOf(needle);
 };
+
+/**
+ * @description: 
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  if (needle === '') return 0
+  for (let i = 0;i < haystack.length;i++) {
+    if (haystack[i] === needle[0]) {
+      if (haystack.substring(i, i + needle.length) === needle)
+      return i
+    }
+  }
+  return -1
+}
