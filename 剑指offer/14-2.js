3848f4f697fee99c8bb74a988cffe86900ebca28
shaoyun <shaoyun@meituan.com>
2020-04-29 23:42:42 +0800

    剪绳子II

--- 剑指offer/14-2.js
+++ 剑指offer/14-2.js
@@ -4,10 +4,11 @@
  * @return {number}
  */
 var cuttingRope = function(n) {
-  // 贪心算法，与上一题不同之处在于大数取模，需要在过程中取模
+  // 贪心算法，与上一题不同之处在于大数取模
   if (n <= 3) return n - 1
   let res = 1
   while (n > 4) {
+    // 重点：需要在过程中取模，否则会溢出
     res = res * 3 % (1e9 + 7)
     n -= 3
   }
