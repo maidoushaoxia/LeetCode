1ca14f6abcecc5cb05d32afaf8add3b4317205f1
shaoyun <shaoyun@meituan.com>
2020-05-28 09:59:05 +0800

    反转链表

--- 剑指offer/24.js
+++ 剑指offer/24.js
@@ -12,12 +12,12 @@
  */
 var reverseList = function(head) {
   // 双指针
-  let cur = head, pre = head.next
-  while (pre.val) {
-    let temp = pre.next
-    pre.next = cur
-    cur = pre
-    pre = temp
+  let cur = head, pre = null
+  while (cur) {
+    let temp = cur.next
+    cur.next = pre
+    pre = cur
+    cur = temp
   }
-
+  return pre
 };
