4102a38938d789f620cc43b8de5f3cfcb9c9c7b5
shaoyun <shaoyun@meituan.com>
2020-06-15 18:46:58 +0800

    栈的压入\弹出序列

--- 剑指offer/31.js
+++ 剑指offer/31.js
@@ -5,15 +5,17 @@
  * @return {boolean}
  */
 var validateStackSequences = function(pushed, popped) {
-  if (!pushed.length) return true
+  if (pushed.length != popped.length) {
+      return false
+  }
   let stack = []
-  let j = 0
-  for (let i = 0; i < pushed.length;i++) {
-    stack.push(pushed[i])
-    while (stack.length && j < popped.length && stack[stack.length - 1] === popped[i]) {
-      stack.pop()
-      j++
-    }
+  let i = 0
+  for (let num of pushed) {
+      stack.push(num)
+      while (stack.length !== 0 && stack[stack.length - 1] === popped[i]) {
+          stack.pop()
+          i++
+      }
   }
   return !stack.length
 };
